da0fcc4497bf19b3be50a3a866a7d76bd7a1690a
Asit Kumar Parida <asitparida@live.in>
2016-04-12 06:18:15 +0530

    FileName Fixes, Adding disable attribute manipulation, Fixing GULP issues

--- Source.DisableContent/dist/js/app.js
+++ Source.DisableContent/dist/js/app.js
@@ -1,8 +1,15 @@
-﻿angular.module('sampleApp', ['utility.disableContent'])
+﻿angular.module('sampleApp', ['utility.disableElement'])
 .controller('sampleCtrl', ["$scope", function ($scope) {
     var self = this;
     self.disable = false
     self.click = function (id) {
         self.disable = !self.disable;
+    }
+    self.clickdisabled = false;
+    self.consolelog = function () {
+        console.log(self);
+    }
+    self.toggleButtonDisabledState = function () {
+        self.clickdisabled = !self.clickdisabled;
     }
 }]);
